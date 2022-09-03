da2fc918c757b1f63abdee326d55aeac2bc8e19c
Xuhao <jq_xuhao@163.com>
2022-09-03 09:19:15 +0800

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { IndexKind } from 'typescript';
+import Button,{ButtonSize,ButtonType} from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Menu defaultIndex={1} onSelect={(index)=>{alert(index)}}>
+          <MenuItem index={1}>
+          Cool link
+          </MenuItem>
+          <MenuItem index={2} disabled>
+          Cool link 2
+          </MenuItem>
+          <MenuItem index={3}>
+          Cool link 3
+          </MenuItem>
+        </Menu>
+        <Menu defaultIndex={1} onSelect={(index)=>{alert(index)}} className={'menu-vertical'}>
+          <MenuItem index={1}>
+          Cool link
+          </MenuItem>
+          <MenuItem index={2} disabled>
+          Cool link 2
+          </MenuItem>
+          <MenuItem index={3}>
+          Cool link 3
+          </MenuItem>
+        </Menu>
+        <Button onClick={()=>console.log('123')}> hello </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large} disabled> Primary 按钮 </Button>
+        <Button btnType={ButtonType.Default} size={ButtonSize.Small}> Primary 按钮 </Button>
+        <Button btnType={ButtonType.Link} href='www.baidu.com' disabled>  Link  </Button>
+        <Button btnType={ButtonType.Danger}> hello </Button>
+        <Button> hello </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
